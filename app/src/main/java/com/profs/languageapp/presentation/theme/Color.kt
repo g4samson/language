1726ee9419278992a25172cf0036fb4b88a94bf3
GrSamson <grigsam14@gmail.com>
2026-01-06 14:07:31 +0300

    colors and fonts

--- app/src/main/java/com/profs/languageapp/presentation/theme/Color.kt
+++ app/src/main/java/com/profs/languageapp/presentation/theme/Color.kt
@@ -2,10 +2,23 @@ package com.profs.languageapp.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val DeepBlue = Color(0xFF410FA3)
+val Blue = Color(0xFF5B7BFE)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Orange = Color(0xFFF76400)
+
+val Green = Color(0xFF5BA890)
+val GreenBlue = Color(0xFF00B5AE)
+
+val LightBlue = Color(0xFFDBF6FF)
+val LightCream = Color(0xFFFFF6EB)
+
+val Red = Color(0xFFD6185D)
+
+val DefaultWhite =Color(0xFFFFFFFF)
+val GrayLight =Color(0xFFE5E5E5)
+val GrayDark =Color(0xFF656872)
+
+val Dark = Color(0xFF080E1E)
+val DarkLight = Color(0xFF1B2336)
+val DarkLighter = Color(0xFF363B44)
--- app/src/main/java/com/profs/languageapp/presentation/theme/Type.kt
+++ app/src/main/java/com/profs/languageapp/presentation/theme/Type.kt
@@ -2,11 +2,21 @@ package com.profs.languageapp.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.profs.languageapp.R
 
-// Set of Material typography styles to start with
+
+val Fredoka = FontFamily(
+    Font(R.font.fredoka_bold, FontWeight.Bold),
+    Font(R.font.fredoka_semibold, FontWeight.SemiBold),
+    Font(R.font.fredoka_medium, FontWeight.Medium),
+    Font(R.font.fredoka_regular, FontWeight.Normal),
+    Font(R.font.fredoka_light, FontWeight.Light),
+
+    )
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -15,20 +25,5 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+
 )
