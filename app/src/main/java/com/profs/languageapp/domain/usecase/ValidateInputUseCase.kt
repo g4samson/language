cb1ea5ae25ee813d3e7a7816a8a0e7298200661f
GrSamson <grigsam14@gmail.com>
2026-02-04 00:48:23 +0300

    regex removed

--- app/src/main/java/com/profs/languageapp/domain/usecase/ValidateInputUseCase.kt
+++ app/src/main/java/com/profs/languageapp/domain/usecase/ValidateInputUseCase.kt
@@ -1,14 +1,15 @@
 package com.profs.languageapp.domain.usecase
 
+import android.util.Log
+import android.util.Patterns
 import javax.inject.Inject
 
 class ValidateInputUseCase @Inject constructor() {
 
-    private val emailPattern =
-        Regex("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
-
     fun isEmailValid(email: String): Boolean {
-        return emailPattern.matches(email)
+        return Patterns.EMAIL_ADDRESS.matcher(email.trim()).matches()
+        Log.d("EMAIL", ">$email< length=${email.length}")
+        Log.d("EMAIL", "${Patterns.EMAIL_ADDRESS.matcher(email.trim()).matches()}")
     }
 
     fun isPasswordValid(password: String): Boolean {
@@ -31,48 +32,3 @@ class ValidateInputUseCase @Inject constructor() {
         return password == confirmation
     }
 }
-
-
-
-
-
-
-
-
-
-//
-//package com.profs.languageapp.domain.usecase
-//
-//import android.content.Context
-//import android.content.Intent
-//import androidx.core.content.FileProvider
-//import jakarta.inject.Inject
-//import java.io.File
-//
-//class OpenPdfUseCase @Inject constructor() {
-//
-//    fun openPdf(context: Context, fileName: String) {
-//        val pdfFile = File(context.filesDir, fileName)
-//
-//        if (!pdfFile.exists()) {
-//            context.assets.open(fileName).use { input ->
-//                pdfFile.outputStream().use { output ->
-//                    input.copyTo(output)
-//                }
-//            }
-//        }
-//
-//        val uri = FileProvider.getUriForFile(
-//            context,
-//            context.packageName + ".fileprovider",
-//            pdfFile
-//        )
-//
-//        val intent = Intent(Intent.ACTION_VIEW).apply {
-//            setDataAndType(uri, "application/pdf")
-//            flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-//        }
-//
-//        context.startActivity(intent)
-//    }
-//} можешь попробовать найти с этим кодом примерно доки на android.developers или что-то что поможет мне понять как пдф открыть
